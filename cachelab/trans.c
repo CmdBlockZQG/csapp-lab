03d12f991ffcf5887b2d12fb51c9291df29a7349
CmdBlock <CmdBlockZQG@outlook.com>
2024-01-18 17:31:46 +0800

    improve cache lab B2

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -14,6 +14,7 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
 void solve_32_32(int A[32][32], int B[32][32]) {
     int bi, bj, i, j;
+    int a0, a1, a2, a3, a4, a5, a6, a7;
     for (bi = 0; bi < 32; bi += 8) {
         for (bj = 0; bj < 32; bj += 8) {
             if (bi == bj) continue;
@@ -24,7 +25,6 @@ void solve_32_32(int A[32][32], int B[32][32]) {
             }
         }
     }
-    int a0, a1, a2, a3, a4, a5, a6, a7;
     for (bi = 0; bi < 32; bi += 8) {
         for (i = 0; i < 8; ++i) {
             a0 = A[bi + i][bi + 0];
@@ -88,70 +88,70 @@ void tp_p1_d(int A[64][64], int B[64][64], int ai, int aj, int ci, int cj) {
     tp_p1(A, B, aj, ai, ai, aj, ci, cj);
 }
 
-void tp_p2(int A[64][64], int B[64][64], int ai, int aj) {
+void tp_p2(int A[64][64], int B[64][64], int a) {
     int i, j;
     for (i = 0; i < 4; ++i) {
         for (j = 0; j < 8; ++j) {
-            B[16 + i][16 + j] = A[ai + i][aj + j];
+            B[a + 8 + i][a + 8 + j] = A[a + i][a + j];
         }
     }
     for (i = 0; i < 4; ++i) {
         for (j = 0; j < 8; ++j) {
-            B[24 + i][24 + j] = A[ai + 4 + i][aj + j];
+            B[a + 16 + i][a + 16 + j] = A[a + 4 + i][a + j];
         }
     }
     for (i = 0; i < 4; ++i) {
         for (j = 0; j < 4; ++j) {
-            B[aj + j][ai + i] = B[16 + i][16 + j];
-            B[aj + j][ai + 4 + i] = B[24 + i][24 + j];
+            B[a + j][a + i] = B[a + 8 + i][a + 8 + j];
+            B[a + j][a + 4 + i] = B[a + 16 + i][a + 16 + j];
         }
     }
     for (i = 0; i < 4; ++i) {
         for (j = 0; j < 4; ++j) {
-            B[aj + 4 + j][ai + i] = B[16 + i][20 + j];
-            B[aj + 4 + j][ai + 4 + i] = B[24 + i][28 + j];
+            B[a + 4 + j][a + i] = B[a + 8 + i][a + 12 + j];
+            B[a + 4 + j][a + 4 + i] = B[a + 16 + i][a + 20 + j];
         }
     }
 }
 
-void tp_p3(int A[64][64], int B[64][64], int ai, int aj) {
+void tp_p3(int A[64][64], int B[64][64]) {
     int i, j;
     for (i = 0; i < 4; ++i) {
         for (j = 0; j < 8; ++j) {
-            B[24 + i][24 + j] = A[ai + i][aj + j];
+            B[56 + i][56 + j] = A[48 + i][48 + j];
         }
     }
     for (i = 0; i < 8; ++i) {
         for (j = 0; j < 4; ++j) {
-            B[aj + i][ai + j] = B[24 + j][24 + i];
+            B[48 + i][48 + j] = B[56 + j][56 + i];
         }
     }
     for (i = 0; i < 4; ++i) {
         for (j = 0; j < 8; ++j) {
-            B[24 + i][24 + j] = A[ai + 4 + i][aj + j];
+            B[56 + i][56 + j] = A[48 + 4 + i][48 + j];
         }
     }
     for (i = 0; i < 8; ++i) {
         for (j = 0; j < 4; ++j) {
-            B[aj + i][ai + 4 + j] = B[24 + j][24 + i];
+            B[48 + i][48 + 4 + j] = B[56 + j][56 + i];
         }
     }
 }
 
-void tp_p4(int A[64][64], int B[64][64], int ai, int aj) {
+void tp_p4(int A[64][64], int B[64][64]) {
     int ci, cj, i, a0, a1, a2, a3;
     for (ci = 0; ci < 8; ci += 4) {
         for (cj = 0; cj < 8; cj += 4) {
             for (i = 0; i < 4; ++i) {
-                a0 = A[ai + ci + i][aj + cj + 0];
-                a1 = A[ai + ci + i][aj + cj + 1];
-                a2 = A[ai + ci + i][aj + cj + 2];
-                a3 = A[ai + ci + i][aj + cj + 3];
+                a0 = A[56 + ci + i][56 + cj + 0];
+                a1 = A[56 + ci + i][56 + cj + 1];
+                a2 = A[56 + ci + i][56 + cj + 2];
+                a3 = A[56 + ci + i][56 + cj + 3];
 
-                B[aj + cj + 0][ai + ci + i] = a0;
-                B[aj + cj + 1][ai + ci + i] = a1;
-                B[aj + cj + 2][ai + ci + i] = a2;
-                B[aj + cj + 3][ai + ci + i] = a3;
+                B[56 + cj + 0][56 + ci + i] = a0;
+                B[56 + cj + 1][56 + ci + i] = a1;
+                B[56 + cj + 2][56 + ci + i] = a2;
+                B[56 + cj + 3][56 + ci + i] = a3;
             }
         }
     }
@@ -160,43 +160,23 @@ void tp_p4(int A[64][64], int B[64][64], int ai, int aj) {
 void solve_64_64(int A[64][64], int B[64][64]) {
     int ci, cj;
     // phase 1
-    for (ci = 0; ci < 64; ci += 32) {
-        for (cj = 0; cj < 64; cj += 32) {
-            tp_p1_d(A, B, ci, cj + 16, 8, 8);
-            tp_p1_d(A, B, ci, cj + 24, 8, 8);
-            tp_p1_d(A, B, ci + 16, cj + 24, 8, 8);
-        }
-    }
-    for (ci = 0; ci < 64; ci += 32) {
-        for (cj = 0; cj < 64; cj += 32) {
-            tp_p1_d(A, B, ci, cj + 8, 16, 16);
-            tp_p1_d(A, B, ci + 8, cj + 24, 16, 16);
-        }
-    }
-    for (ci = 0; ci < 64; ci += 32) {
-        for (cj = 0; cj < 64; cj += 32) {
-            tp_p1_d(A, B, ci + 8, cj + 16, 24, 24);
+    for (ci = 0; ci < 64; ci += 8) {
+        for (cj = ci + 8; cj < 64; cj += 8) {
+            if (ci == 0) {
+                if (cj < 56) tp_p1_d(A, B, ci, cj, 56, 56);
+                else tp_p1_d(A, B, ci, cj, 48, 48);
+            } else {
+                tp_p1_d(A, B, ci, cj, 0, 0);
+            }
         }
     }
     // phase 2
-    for (ci = 0; ci < 64; ci += 32) {
-        for (cj = 0; cj < 64; cj += 32) {
-            tp_p2(A, B, ci, cj);
-            tp_p2(A, B, ci + 8, cj + 8);
-        }
+    for (ci = 0; ci < 48; ci += 8) {
+        tp_p2(A, B, ci);
     }
     // phase 3
-    for (ci = 0; ci < 64; ci += 32) {
-        for (cj = 0; cj < 64; cj += 32) {
-            tp_p3(A, B, ci + 16, cj + 16);
-        }
-    }
-    // phase 4
-    for (ci = 0; ci < 64; ci += 32) {
-        for (cj = 0; cj < 64; cj += 32) {
-            tp_p4(A, B, ci + 24, cj + 24);
-        }
-    }
+    tp_p3(A, B);
+    tp_p4(A, B);
 }
 
 void solve_61_67(int A[61][67], int B[61][67]) {
